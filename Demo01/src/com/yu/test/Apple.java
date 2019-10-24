02ce940ec1ecaa2afaaa917489222f64bf162ef4
chenyu5456 <545641154@qq.com>
2019-10-24 22:01:44 +0800

    lambda 练习

--- Demo01/src/com/yu/test/Apple.java
+++ Demo01/src/com/yu/test/Apple.java
@@ -1,4 +1,22 @@
 package com.yu.test;
 
 public class Apple {
+    private String color;
+    private Integer weight;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
 }
