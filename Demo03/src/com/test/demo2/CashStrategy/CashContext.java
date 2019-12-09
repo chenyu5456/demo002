1c2971000a73ca904e3ce4431f34ca89cf60bf93
chenyu5456 <545641154@qq.com>
2019-12-09 22:12:21 +0800

    策略加简单工厂

--- Demo03/src/com/test/demo2/CashStrategy/CashContext.java
+++ Demo03/src/com/test/demo2/CashStrategy/CashContext.java
@@ -1,18 +1,28 @@
 package com.test.demo2.CashStrategy;
 
+import com.test.demo2.Factory.CashNormal;
+import com.test.demo2.Factory.CashRebate;
+import com.test.demo2.Factory.CashReturn;
 import com.test.demo2.Factory.CashSuper;
 
-/**
- * 上下文
- */
 public class CashContext {
-    private CashSuper cashSuper;
+    public CashSuper cashSuper;
 
-    public CashContext(CashSuper cashSuper) {
-        this.cashSuper = cashSuper;
+    public CashContext(String type){
+        switch (type){
+            case "正常收费":
+                cashSuper = new CashNormal();
+                break;
+            case "满300返100":
+                cashSuper = new CashReturn(300, 100);
+                break;
+            case "打8折":
+                cashSuper = new CashRebate(0.8);
+                break;
+        }
     }
 
-    public double GetResult(double money){
-        return cashSuper.acceptCash(money);
+    public double GetResult(double meney){
+        return cashSuper.acceptCash(meney);
     }
 }
